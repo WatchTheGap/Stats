c0554669626270dfcac588dab39ed2dc86e1b82b
Sara Basile <sarabasile@Saras-MacBook-Air.local>
2017-03-06 15:10:44 -0500

    fixed fetch url to take username input from terminal

--- src/main.js
+++ src/main.js
@@ -1,20 +1,20 @@
 let fetch = require("node-fetch");
 let promise = fetch(
-  "https://api.github.com/users/jakerella",
-  // "https://api.github.com/users/jakerella/repos",
+  "https://api.github.com/users/" + process.argv[2],
+  // "https://api.github.com/users/WatchTheGap/repos",
 
-  {
-    method: "GET",
+  { method: "GET",
     headers: {
-      Authorization: "token " + process.argv[2]
-  }
-});
+    Authorization: "token " + process.argv[3]
+    }
+  });
 
 promise.then(function handler(response) {
   console.log(response.status);
   if (response.status > 199 && response.status <300) {
     response.json().then(function printData(data) {
       console.log(data.name, data.location);
+      // console.log(data, data.stargazers_count);
       // console.log(data.stargazers_count);
     });
   } else {
