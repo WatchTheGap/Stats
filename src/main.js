d6f2cbcc518c829f36b6275d6a2456136d299ecb
Sara Basile <sarabasile@Saras-MacBook-Air.local>
2017-03-07 08:55:47 -0500

    safety push before submission

--- src/main.js
+++ src/main.js
@@ -37,19 +37,18 @@ promiseTwo.then(function handler(response) {
   if (response.status > 199 && response.status < 300) {
     response.json().then(function printData(data) {
       console.log(data.length);
+
         data.forEach(function countStars(each) {
         let starCount = new Number(each.stargazers_count);
         stars = stars + starCount;
         if (starCount >= maxStars)  {
           maxStars = starCount;
-
         }
-        // console.log(each.name, starCount);
+        console.log(each.name, starCount);
+
         // console.log(maxStars);
         // console.log(starCount);
         // console.log("Total stars: " + stars);
-        //
-
         });
 
       });
@@ -63,22 +62,24 @@ promiseTwo.then(function handler(response) {
 
 // ************
 
+// //
+// let promiseThree = fetch(
+//   "https://api.github.com/users/" +process.argv[2]+ "/repos",
+//   { method: "GET",
+//     headers: {
+//     authorization: "token " + process.argv[3],
+//     }
+//   });
+//   promiseThree.then(function handler(response) {
+//     console.log(response.status);
+//     if (response.status > 199 && response.status < 300) {
+//       response.json().then(function printData(data) {
+//         data.indexOf()
+//       console.log(data[0].contributors_url);
 //
-let promiseThree = fetch(
-  "https://api.github.com/users/" +process.argv[2]+ "/repos",
-  { method: "GET",
-    headers: {
-    authorization: "token " + process.argv[3],
-    }
-  });
-  promiseThree.then(function handler(response) {
-    console.log(response.status);
-    if (response.status > 199 && response.status < 300) {
-      response.json().then(function printData(data) {
-      console.log(data[0].contributors_url);
-      });
-    }
-      else {
-        console.log("Insert Problem Here", response.status);
-      }
-  });
+//       });
+//     }
+//       else {
+//         console.log("Insert Problem Here", response.status);
+//       }
+//   });
