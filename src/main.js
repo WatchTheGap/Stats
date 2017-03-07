d09b49a5af16b10bdf5b69519a1527d591ad780d
Sara Basile <sarabasile@Saras-MacBook-Air.local>
2017-03-06 19:32:03 -0500

    located contributor url through data array

--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
 let fetch = require("node-fetch");
-let promise = fetch(
-  // "https://api.github.com/users/" + process.argv[2],
-  "https://api.github.com/users/" +process.argv[2]+ "/repos",
+let promiseOne = fetch(
+  "https://api.github.com/users/" + process.argv[2],
+  { method: "GET",
+    headers: {
+    authorization: "token " + process.argv[3],
+    }
+  });
 
+promiseOne.then(function handler(response) {
+  console.log(response.status);
+  if (response.status > 199 && response.status < 300) {
+    response.json().then(function printData(data) {
+    console.log(data.name, data.location);
+    });
+  }
+    else {
+      console.log("Insert Problem Here", response.status);
+    }
+});
+
+// *******
+
+let promiseTwo = fetch(
+  "https://api.github.com/users/" +process.argv[2]+ "/repos",
   { method: "GET",
     headers: {
     authorization: "token " + process.argv[3],
@@ -12,30 +32,53 @@ let promise = fetch(
 let stars = 0;
 let maxStars = 0;
 
-promise.then(function handler(response) {
+promiseTwo.then(function handler(response) {
   console.log(response.status);
   if (response.status > 199 && response.status < 300) {
     response.json().then(function printData(data) {
-      // console.log(data.name, data.location); //<---keep for question 1
-      // console.log(data[0]);
       console.log(data.length);
-
-      data.forEach(function countStars(each) {
+        data.forEach(function countStars(each) {
         let starCount = new Number(each.stargazers_count);
         stars = stars + starCount;
         if (starCount >= maxStars)  {
           maxStars = starCount;
-        console.log(each.name, starCount);
-        }
-        console.log(maxStars);
-        // console.log(data.indexOf(maxStars));
 
-        console.log(starCount);
-        console.log(stars);
+        }
+        // console.log(each.name, starCount);
+        // console.log(maxStars);
+        // console.log(starCount);
+        // console.log("Total stars: " + stars);
+        //
+
         });
+
       });
 
   } else {
     console.log("Insert Problem Here", response.status);
   }
+
+
 });
+
+// ************
+
+//
+let promiseThree = fetch(
+  "https://api.github.com/users/" +process.argv[2]+ "/repos",
+  { method: "GET",
+    headers: {
+    authorization: "token " + process.argv[3],
+    }
+  });
+  promiseThree.then(function handler(response) {
+    console.log(response.status);
+    if (response.status > 199 && response.status < 300) {
+      response.json().then(function printData(data) {
+      console.log(data[0].contributors_url);
+      });
+    }
+      else {
+        console.log("Insert Problem Here", response.status);
+      }
+  });
